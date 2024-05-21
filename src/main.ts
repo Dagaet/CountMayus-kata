7851c22ca026e57227e7d91b46aa39c365d12cc3
Dagaet <alojera15@gmail.com>
2024-05-21 09:29:40 +0100

    Test(Green): obtainMayusPositionsInString should register an array with positions when there are mayus

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-export function obtainMayusPositionsInString(text: string): string[] {
-  return []
+export function obtainMayusPositionsInString(text: string): number[] {
+  const numbers: number[] = []
+
+  let regex: RegExp = /[A-Z]/g
+  // let output = text.match(regex)
+  // output?.forEach((mayus) => {
+  //   numbers.push(text.indexOf(mayus))
+  // })
+
+  for (let position = 0; position < text.length; position++) {
+    if (regex.test(text[position])) {
+      numbers.push(position)
+    }
+  }
+
+  return numbers
 }
